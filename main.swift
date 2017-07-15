931b23df1aaef2ac4a5d753eccf7e3f322598329
Mike O'Brien <mpobrien005@gmail.com>
2017-07-15 18:21:14 -0400

    expression parsing sorta working.

--- main.swift
+++ main.swift
@@ -13,7 +13,53 @@ extension Character
     }
 }
 
-enum Token {
+public struct Identifier : Expression {
+	var name: String
+}
+
+public struct IntegerLiteral : Expression {
+	var value: Int
+}
+
+
+public struct StringLiteral : Expression {
+	var value: String
+}
+
+public class Stuff : Expression {
+	var tok: Token
+	init(tok: Token){
+		self.tok = tok
+	}
+}
+
+public struct ParseError: Error {
+	let message: String
+}
+
+enum Precedence : Int {
+	case LOWEST = 0
+	case EQUALS = 1
+	case LESSGREATER = 2
+	case SUM  = 3
+	case PRODUCT = 4
+	case PREFIX  = 5
+	case CALL = 6
+	func toString() -> String{
+		switch self {
+			case .LOWEST: return "LOWEST"
+			case .EQUALS: return "EQUALS"
+			case .LESSGREATER: return "LESSGREATER"
+			case .SUM : return "SUM"
+			case .PRODUCT: return "PRODUCT"
+			case .PREFIX : return "PREFIX"
+			case .CALL: return "CALL"
+		}
+	}
+}
+
+
+public enum Token {
     case EOF
     case EQ
     case LPAREN
@@ -23,6 +69,7 @@ enum Token {
     case ASTERISK
     case BANG
     case COMMA
+	case COMMENT(String)
     case GT
     case GTE
     case IDENT(String)
@@ -55,6 +102,69 @@ enum Token {
     case RETURN
     case LOCAL
     case STRING(String)
+
+	func opPrecedence() -> Precedence {
+		switch self {
+			case .EQ: return Precedence.EQUALS
+			case .NOTEQ: return Precedence.EQUALS
+            case .LT: return Precedence.LESSGREATER
+    		case .GT: return Precedence.LESSGREATER
+			case .PLUS: return Precedence.SUM
+			case .MINUS: return Precedence.SUM
+			case .SLASH: return Precedence.PRODUCT
+			case .ASTERISK: return Precedence.PRODUCT
+		default:
+			return Precedence.LOWEST
+		}
+	}
+
+	func toString() -> String {
+		switch self {
+		case .EOF: return "EOF"
+		case .EQ: return "EQ"
+		case .LPAREN: return "LPAREN"
+		case .RPAREN: return "RPAREN"
+		case .UNKNOWN: return "UNKNOWN"
+		case .ASSIGN: return "ASSIGN"
+		case .ASTERISK: return "ASTERISK"
+		case .BANG: return "BANG"
+		case .COMMA: return "COMMA"
+		case let .COMMENT(x): return "COMMENT[" + x + "]"
+		case .GT: return "GT"
+		case .GTE: return "GTE"
+		case let .IDENT(x): return "IDENT[" + x + "]" 
+		case .ILLEGAL: return "ILLEGAL"
+		case let .INT(x): return "INT[" + x + "]"
+		case .LBRACE: return "LBRACE"
+		case .LET: return "LET"
+		case .LT: return "LT"
+		case .LTE: return "LTE"
+		case .MINUS: return "MINUS"
+		case .NOTEQ: return "NOTEQ"
+		case .NIL: return "NIL"
+		case .PLUS: return "PLUS"
+		case .RBRACE: return "RBRACE"
+		case .SEMICOLON: return "SEMICOLON"
+		case .SLASH: return "SLASH"
+		case .WHILE: return "WHILE"
+		case .DO: return "DO"
+		case .IF: return "IF"
+		case .ELSE: return "ELSE"
+		case .ELSEIF: return "ELSEIF"
+		case .THEN: return "THEN"
+		case .NOT: return "NOT"
+		case .FOR: return "FOR"
+		case .REPEAT: return "REPEAT"
+		case .TILDE: return "TILDE"
+		case .UNTIL: return "UNTIL"
+		case .FUNCTION: return "FUNCTION"
+		case .END: return "END"
+		case .RETURN: return "RETURN"
+		case .LOCAL: return "LOCAL"
+		case let .STRING(x):
+			return "STRING[" + x + "]"
+		}
+	}
     
     func isEOF() -> Bool{
         switch self {
@@ -64,6 +174,23 @@ enum Token {
             return false
         }
     }
+}
+
+protocol Statement {
+}
+
+protocol Expression {
+}
+
+struct PrefixExpression : Expression {
+	var Operator: String
+	var rhs : Expression
+}
+
+struct InfixExpression : Expression {
+	var lhs: Expression
+	var Operator: String
+	var rhs: Expression
 }
 
 let EscapeSequences : Dictionary<Character, Character> = [
@@ -100,6 +227,190 @@ var keywords : Dictionary<String, Token> = [
     "local": Token.LOCAL,
 ]
 
+
+typealias Program = Array<Statement>
+
+public struct AssignStatement : Statement{
+	var ident : String
+	var rhs : Expression
+}
+
+public struct ExpressionStatement : Statement {
+	var expression : Expression
+}
+
+
+public class Parser {
+	let lexer : Lexer
+	var curToken: Token = Token.ILLEGAL
+	var peekToken: Token = Token.ILLEGAL 
+	init(lexer : Lexer){
+		self.lexer = lexer;
+		self.nextToken()
+		self.nextToken()
+	}
+
+/*
+var precedences = map[token.TokenType]int{ token.EQ: EQUALS,
+token.NOT_EQ: EQUALS,
+    token.LT:
+    token.GT:
+    token.PLUS:
+    token.MINUS:    SUM,
+    token.SLASH:    PRODUCT,
+    token.ASTERISK: PRODUCT,
+}
+// [...]
+func (p *Parser) peekPrecedence() int {
+if p, ok := precedences[p.peekToken.Type]; ok {
+return p }
+return LOWEST }
+*/
+
+
+
+	/*
+	func expectPeek(tokenType: Token) {
+		if  case self.peekToken == tokenType {
+		}
+		if p.peekTokenIs(t) {
+		    p.nextToken()
+				   return true } else {
+					   return false }
+			*/
+	func nextToken() {
+		self.curToken = self.peekToken
+		self.peekToken = self.lexer.nextToken()
+	}
+
+	func parsePrefix() throws -> Expression {
+		switch self.curToken {
+			case let Token.IDENT(ident):
+				return Identifier(name: ident)
+			case let Token.INT(integer):
+				guard let x = Int(integer) else {
+					throw ParseError(message:"Cannot parse '\(integer)' as an integer")
+				}
+				return IntegerLiteral(value: x)
+			case let Token.STRING(x):
+				return StringLiteral(value: x)
+			default:
+				throw ParseError(message: "No prefix expression parse function for " + self.curToken.toString())
+		}
+	}
+
+	func parseInfix(left: Expression) throws -> Expression? {
+		var opcode : String
+		switch self.peekToken {
+			case Token.PLUS:
+				opcode = "+"
+			case Token.MINUS:
+				opcode = "+"
+			case Token.ASTERISK:
+				opcode = "+"
+			case Token.SLASH:
+				opcode = "+"
+			case Token.EQ:
+				opcode = "=="
+			case Token.NOTEQ:
+				opcode = "~="
+			case Token.LT:
+				opcode = "<"
+			case Token.GT:
+				opcode = ">"
+			default:
+				return nil
+		}	
+		self.nextToken()
+		let prec = self.curToken.opPrecedence()
+		self.nextToken()
+		let rhs = try self.parseExpression(prec:prec)
+		return InfixExpression(lhs: left, Operator: opcode, rhs: rhs)
+	}
+
+	func parseExpression(prec: Precedence) throws -> Expression {
+		var left = try self.parsePrefix()
+		while true {
+			if case Token.SEMICOLON = self.peekToken {
+				break;
+			}
+			if case Token.EOF = self.peekToken {
+				break;
+			}
+			if prec.rawValue >= self.peekToken.opPrecedence().rawValue {
+				break;
+			}
+			let infixLeft = try self.parseInfix(left: left) 
+			if infixLeft == nil {
+				return left
+			}else{
+				left = infixLeft!
+			}
+		}
+
+		return left;
+		
+
+/*
+		let left = try self.curToken.parsePrefix()
+		for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+			 infix := p.infixParseFns[p.peekToken.Type]
+if infix == nil {
+return leftExp }
+           p.nextToken()
+           leftExp = infix(leftExp)
+       }
+*/
+
+
+
+		switch self.curToken {
+			case let Token.IDENT(ident):
+				return Identifier(name: ident)
+			default:
+				throw ParseError(message:"no parse expression for " + self.curToken.toString());
+		}
+	}
+
+	func parseAssignStatement(lhs : String) throws -> AssignStatement {
+		self.nextToken() // consume the lhs
+		self.nextToken() // consume the '=' token.
+
+		return AssignStatement(
+			ident: lhs, 
+			rhs: try self.parseExpression(prec:Precedence.LOWEST)
+		)
+	}
+
+	func parseExpressionStatement() throws -> ExpressionStatement {
+		return ExpressionStatement(expression: Stuff(tok:Token.ILLEGAL))
+		
+	}
+
+	func parseStatement() throws -> Statement {
+		switch self.curToken {
+			case let Token.IDENT(ident):
+				if case Token.ASSIGN = self.peekToken {
+					return try self.parseAssignStatement(lhs: ident)
+				}
+
+				throw ParseError(message:":(")
+		default:
+			throw ParseError(message: "unexpected token: " + self.curToken.toString())
+		}
+	}
+
+	func parseProgram() throws -> Program {
+		var program = Program()
+		while !self.curToken.isEOF() {
+			let s = try self.parseStatement()
+			program.append(s)
+			self.nextToken()
+		}
+		return program
+	}
+
+}
 
 public class Lexer {
     var input : String
@@ -148,6 +459,13 @@ public class Lexer {
             tok = Token.RBRACE;
         case ",":
             tok = Token.COMMA;
+        case "-":
+            if self.peekChar() == "-" {
+                self.readChar()
+                tok = Token.COMMENT(self.readComment())
+            }else{
+                tok = Token.MINUS;
+            }
         case "+":
             tok = Token.PLUS;
         case "<":
@@ -201,13 +519,24 @@ public class Lexer {
             out += String(self.ch)
         }
         return out
-        
     }
     
     func skipWhitespace(){
         while self.ch.isWhitespace(){
             self.readChar()
         }
+    }
+
+    func readComment() -> String {
+        var out : String = ""
+        while true {
+            self.readChar()
+			if self.ch == "\n" || self.ch == Character("\u{0000}") {
+				break;
+			}
+            out += String(self.ch)
+        }
+        return out
     }
     
     func readIdentifier() -> String {
@@ -252,24 +581,34 @@ public class Lexer {
     }
 }
 
-var input = "if num > 40 then\n" + 
+/*
+var input2 = "if num > 40 then\n" + 
     "print('over 40')\n" +
     "elseif s ~= 'walternate' then\n" +
-    "io.write('not over 40\n')\n" +
+    "io.write('not over 40\n') -- hey sup\n" +
     "else\n" + 
     "thisIsGlobal = 5\n" +
     "local line = io.read()\n" +
     "print('Winter is coming, ' .. line)\n" +
 "end" 
-let lexer = Lexer(input: input)
+*/
 
+var input =  "x = 22 + 34"
+
+let lex2 = Lexer(input: input)
 var tok : Token
-var x : Int = 0
 repeat {
-    tok = lexer.nextToken()
+    tok = lex2.nextToken()
     print("x:", tok)
-    x+=1;
 } while !tok.isEOF()
 
-lexer.readChar()
+
+
+
+
+
+let lexer = Lexer(input: input)
+let parser = Parser(lexer: lexer)
+let program = try parser.parseProgram()
+print(program)
 
